516336cf348953601a47e3de28d6b695602ad86c
Martin Beentjes <martin.beentjes@join.cc>
2022-12-04 21:36:03 +0100

    Add day 1

--- day1/src/main/kotlin/me/beentjes/aoc/Main.kt
+++ day1/src/main/kotlin/me/beentjes/aoc/Main.kt
@@ -1,6 +1,21 @@
 package me.beentjes.aoc;
 
-//main function
-fun main(args: Array<String>) {
-    println("Hello World!");
+
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val input = Main::class.java.getResource("/input.txt")?.readText()
+            val elves = input?.split("\n\n");
+            val elfWithMostCalories = elves?.mapIndexed { index, caloriesPerElf ->
+                val calories = caloriesPerElf.split("\n").map { it.toInt() }.reduce(Int::plus)
+                index to calories
+            }?.sortedByDescending { it.second }!!
+            val topThreeCalories = elfWithMostCalories[0].second + elfWithMostCalories[1].second +elfWithMostCalories[2].second
+
+            println("Elf  with most calories: $topThreeCalories")
+
+        }
+
+    }
 }
